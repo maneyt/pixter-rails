46bb9a2cec83fc8155ba03b4e055dbb2328da5f8
Thomas Maney <thomasjmaney@gmail.com>
2014-06-19 11:06:20 -0400

    Add generated Monban files, display validation errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
- #get "/" => "galleries#index"
- #get "/galleries/new" => "galleries#new"
- #get "/galleries/:id" => "galleries#show"
- #post "/galleries" => "galleries#create"
- #get "/galleries/:id/edit" => "galleries#edit"
- #patch "/galleries/:id" => "galleries#update"
- #delete "/galleries/:id" => "galleries#destroy"
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 
   root to:"galleries#index"  #to generate the regular index page
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617143809) do
+ActiveRecord::Schema.define(version: 20140619133653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20140617143809) do
   create_table "images", force: true do |t|
     t.integer "gallery_id"
     t.string  "url"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
