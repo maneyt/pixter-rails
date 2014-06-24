ea21eecef886ee4f9fe22d405376101782b08358
Thomas Maney <thomasjmaney@gmail.com>
2014-06-24 10:58:54 -0400

    Display group index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,22 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
 
+    if @group.save
+      redirect_to :groups
+    else 
+      render :new
+    end
+  end
+
+  def index
+    @groups = Group.all
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :description)
   end
 end
