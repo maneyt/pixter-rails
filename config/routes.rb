d93c118771c7baf4559662917edc13e02e75db4c
Thomas Maney <thomasjmaney@gmail.com>
2014-06-24 10:24:51 -0400

    form for creating new group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :groups, only: [:new, :create]
 
   root to:"galleries#index"
 
